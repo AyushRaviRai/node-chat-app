4bc6282f04efcb0e808198f79ffdc6e57e149f78
Ayush Ravi <ayushravirai@gmail.com>
2018-06-10 16:05:54 +0530

    socket ke shuruwat ki hain maza aaya

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 // set up static web pages
 var publicPath = path.join(__dirname, '/../public');
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('new user connected ');
+    socket.on('disconnect', (socket) => {
+        console.log("Client disconnected");
+    });
+});
+
+
+
+server.listen(port , () => {
     console.log("Server is up and running ====> ")
 });
